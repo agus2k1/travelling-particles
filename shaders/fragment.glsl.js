8542d88c1a420c5da6ea63976cb6b3f378bef2d2
West <agus2kcoin@outlook.com>
2023-04-03 23:36:01 -0300

    Update particles color

--- shaders/fragment.glsl.js
+++ shaders/fragment.glsl.js
@@ -4,7 +4,25 @@ const fragmentShader = /* glsl */ `
     varying float vOpacity;
 
     void main() {
-        gl_FragColor = vec4(1.) * vOpacity;
+        vec2 uv = vec2(gl_PointCoord.x, 1. - gl_PointCoord.y);
+        vec2 cUV = 2. * uv - 1.; // Centralized UV
+
+        vec3 originalColor = vec3(4. / 255., 10. / 255., 20. / 255.); // Particles color
+
+        vec4 color = vec4(0.08 / length(cUV));
+
+        color.rgb = min(vec3(10.), color.rgb);
+
+        color.rgb *= originalColor * 120.; // Change
+
+        color *= vOpacity;
+
+        color.a = min(1., color.a) * 10.; // Change
+
+        float disc = length(cUV);
+
+        gl_FragColor = vec4(1. - disc, 0., 0., 1.) * vOpacity;
+        gl_FragColor = vec4(color.rgba);
     }
 `;
 
